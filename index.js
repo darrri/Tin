2eb68f33b46460b06ae5a832cee44d56ecab104b
darrri <56608714+darrri@users.noreply.github.com>
2021-09-22 22:50:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
-  return res.end(`Hello!v`);
+  return res.end(`Hello!v1`);
 });
 
 server.listen(PORT, () => {
